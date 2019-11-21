3b5011bf6a05c91845ad6ef4bbda2b1e35c287ad
blsonalib <blsonalib@gmail.com>
2019-11-21 17:36:50 +0530

    Add: Password should have one upperCase

--- userRegistration.sh
+++ userRegistration.sh
@@ -7,7 +7,10 @@ lastName="^[A-Z]+[a-z]{3,}$"
 email="^[a-z]+[A-Za-z0-9]+(.[a-z]*)?+[@]{1}+[a-z]{1,}+[.]{1}+[a-z]{2,4}+(.[a-z]{2}*)?$"
 mobileFormate="^[0-9]{2,3}[: :][0-9]{10}$"
 password1="^[0-9a-zA-Z]{8,}$"
-if [[ $userData =~ $password1 ]];
+#password2="^(([0-9a-zA-Z].*)?{8,})+([A-Z].*))?$"
+password2="^(([0-9a-zA-Z].*)?{8,})+([A-Z].*))?$"
+
+if [[ $userData =~ $password2 ]];
 then 
     echo Valid;
 else
