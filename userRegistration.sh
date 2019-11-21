4e5b6ce944b13e827b3e4c2fe56520bc893af281
blsonalib <blsonalib@gmail.com>
2019-11-21 17:51:45 +0530

    Refactor to  add while loop

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "********USER-REGISTRATION********"
-read userData
+#read userData
 firstName="^[A-Z]+[a-z]{3,}$"
 lastName="^[A-Z]+[a-z]{3,}$"
 email="^[a-z]+[A-Za-z0-9]+(.[a-z]*)?+[@]{1}+[a-z]{1,}+[.]{1}+[a-z]{2,4}+(.[a-z]{2}*)?$"
 mobileFormate="^[0-9]{2,3}[: :][0-9]{10}$"
 password1="^[0-9a-zA-Z]{8,}$"
-#password2="^(([0-9a-zA-Z].*)?{8,})+([A-Z].*))?$"
+
 password2="^(([0-9a-zA-Z].*)?{8,})+([A-Z].*))?$"
 
+while [ true ]
+do
+read userData
 if [[ $userData =~ $password2 ]];
 then 
     echo Valid;
 else
     echo Invalid;
 fi
-
+done
 
