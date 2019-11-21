dfbf4bda0e6e7541ed6d613958829c35b9921f89
blsonalib <blsonalib@gmail.com>
2019-11-21 10:51:45 +0530

    Add : Firstname start with capital latter with minimum three character

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "********USER-REGISTRATION********"
+read userData
+firstName="^[A-Z]+[a-z]{3,}$"
+if [[ $userData =~ $firstName ]];
+then 
+    echo Valid;
+else
+    echo Invalid;
+fi
+
+
