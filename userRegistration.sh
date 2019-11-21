f94181af3b01e8447895c60d9244a0083e23f85b
blsonalib <blsonalib@gmail.com>
2019-11-21 12:47:05 +0530

    Add: Password rule1-minimum 8 characters

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,7 +6,8 @@ firstName="^[A-Z]+[a-z]{3,}$"
 lastName="^[A-Z]+[a-z]{3,}$"
 email="^[a-z]+[A-Za-z0-9]+(.[a-z]*)?+[@]{1}+[a-z]{1,}+[.]{1}+[a-z]{2,4}+(.[a-z]{2}*)?$"
 mobileFormate="^[0-9]{2,3}[: :][0-9]{10}$"
-if [[ $userData =~ $mobileFormate ]];
+password1="^[0-9a-zA-Z]{8,}$"
+if [[ $userData =~ $password1 ]];
 then 
     echo Valid;
 else
