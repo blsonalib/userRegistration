60cb05dd062b5786ccbb7aa660d2f1b98e00d31e
blsonalib <blsonalib@gmail.com>
2019-11-21 18:51:31 +0530

    Add: Should have at least 1 numeric number in the password

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "********USER-REGISTRATION********"
-#read userData
+
 firstName="^[A-Z]+[a-z]{3,}$"
 lastName="^[A-Z]+[a-z]{3,}$"
 email="^[a-z]+[A-Za-z0-9]+(.[a-z]*)?+[@]{1}+[a-z]{1,}+[.]{1}+[a-z]{2,4}+(.[a-z]{2}*)?$"
 mobileFormate="^[0-9]{2,3}[: :][0-9]{10}$"
 password1="^[0-9a-zA-Z]{8,}$"
-
 password2="^(([0-9a-zA-Z].*)?{8,})+([A-Z].*))?$"
-
+password3="^((([a-zA-Z].*)?{8,})+([0-9].*))?$"
 while [ true ]
 do
 read userData
-if [[ $userData =~ $password2 ]];
+if [[ $userData =~ $password3 ]];
 then 
     echo Valid;
 else
