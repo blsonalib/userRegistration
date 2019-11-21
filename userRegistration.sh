204a9b57bc2b685546bec2fe83ec85147119c4b6
blsonalib <blsonalib@gmail.com>
2019-11-21 12:28:34 +0530

    Add: Mobile number in pre-defined format

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,7 +5,8 @@ read userData
 firstName="^[A-Z]+[a-z]{3,}$"
 lastName="^[A-Z]+[a-z]{3,}$"
 email="^[a-z]+[A-Za-z0-9]+(.[a-z]*)?+[@]{1}+[a-z]{1,}+[.]{1}+[a-z]{2,4}+(.[a-z]{2}*)?$"
-if [[ $userData =~ $email ]];
+mobileFormate="^[0-9]{2,3}[: :][0-9]{10}$"
+if [[ $userData =~ $mobileFormate ]];
 then 
     echo Valid;
 else
